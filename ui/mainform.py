a7d3ccc3d5b2b32b790e9055fea43b9fe0b90c79
Snyp code <snypcode@email.com>
2018-07-04 09:20:44 +0300

    upload

---
+++ ui/mainform.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainform.ui'
+#
+# Created by: PyQt5 UI code generator 5.10.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainForm(object):
+    def setupUi(self, MainForm):
+        MainForm.setObjectName("MainForm")
+        MainForm.resize(580, 412)
+
+        self.retranslateUi(MainForm)
+        QtCore.QMetaObject.connectSlotsByName(MainForm)
+
+    def retranslateUi(self, MainForm):
+        _translate = QtCore.QCoreApplication.translate
+        MainForm.setWindowTitle(_translate("MainForm", "tomato"))
+
