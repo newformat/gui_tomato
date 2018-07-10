25961aa90e1e2b8df9350e065bd02d454161e561
Snyp code <snypcode@email.com>
2018-07-10 18:20:33 +0300

    update

--- run.py
+++ run.py
@@ -11,15 +11,46 @@ class MainTomato(QtWidgets.QMainWindow, Ui_MainForm):
     def __init__(self):
         super().__init__()
         self.setupUi(self) # init GUI
-        self.tt = QtGui.QStandardItemModel(self)
+        # отключено по умолчанию
+        self.label_dotsTime.setVisible(False)
+        self.label_TypeInterval.setVisible(False)
+        self.lcdNumber_Minute.setVisible(False)
+        self.lcdNumber_Second.setVisible(False)
 
+        # события по умолчанию
+        # вкладка "Работа"
+        self.horizontalSlider_workMinute.sliderMoved[int].connect(self.event_WorkMinute)
+        self.checkBox_WorkOff.stateChanged.connect(self.event_checkBox_WorkOff)
+
+        self.tt = QtGui.QStandardItemModel(self)
         self.pushButton_Reset.clicked.connect(self.itemList)
+
+
+    # события - вкладка "работа"
+    def event_WorkMinute(self, minute):
+        self.spinBox_WorkMinute.setValue(minute)
+
+
+    def event_checkBox_WorkOff(self, value):
+        if value == 2:
+            self.horizontalSlider_workMinute.setEnabled(False)
+            self.checkBox_WorkOff.setText('off')
+        else:
+            self.horizontalSlider_workMinute.setEnabled(True)
+            self.checkBox_WorkOff.setText('on')
+
+
+# ДОПИСАТЬ БАЗОВЫЕ СОБЫТИЯ ДЛЯ ОСНОВНОГО ОФОРМЛЕНИЯ.
+
+
 
 
     def itemList(self):
         item =  QtGui.QStandardItem("pushButton_2")
         self.tt.appendRow(item)
         self.listView_History.setModel(self.tt)
+
+
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
